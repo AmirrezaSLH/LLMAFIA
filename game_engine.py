c68d8fac01d99f174ce5449bdf1b88e917f0f093
Amirreza Salehi <90405958+AmirrezaSLH@users.noreply.github.com>
2025-02-22 10:48:27 -0500

    Test 0

---
+++ game_engine.py
@@ -0,0 +1,70 @@
+
+
+class Agent:
+    def __init__(self):
+        return
+
+
+class ChatGPTClient:
+    def __init__(self, api_key, model='gpt-4o-mini'):
+        
+        self.client = OpenAI(api_key=api_key)
+        self.model = model
+
+    def prompt_engineer(self, user_query='', context='', general_context='', historical_context = ''):
+        
+        """Creates a structured prompt for better AI responses."""
+        
+        prompt = (
+            f"GPT (You) Role: {general_context}\n"
+            f"Information (About Amirreza Salehi, remember more recent life experiences are more relevant as answers): {context}\n"
+            f"History of The Converstation (Between GPT and User): {historical_context}\n"
+            f"Answer this question (from the User) in a way that aligns with your role, taking into account the provided information and the context of the previous conversation: {user_query}"
+        )
+        
+        return prompt
+
+    def call_chatgpt(self, query):
+        
+        """Calls the OpenAI API with the given query."""
+        
+        chat_completion = self.client.chat.completions.create(
+            messages=[{"role": "user", "content": query}],
+            model=self.model,
+        )
+        
+        return chat_completion
+
+    def process_question(self, user_query='', context='', general_context='', history = [ ('', '') ]):
+        
+        """Handles full query processing, including prompt engineering and API call."""
+        
+        historical_context = self.historical_context_engineer(history)
+        prompt = self.prompt_engineer(user_query, context, general_context, historical_context)
+        gpt_response = self.call_chatgpt(prompt)
+        
+        response = gpt_response.choices[0].message.content
+        
+        return response
+
+
+class init_GAME:
+    def role_assignment():
+        return
+    
+    def load_rules():
+        return
+    
+    def test():
+        return False
+    
+GAME_OVER = False
+nihgt_counter = 0
+
+Print("GitHub Test")
+Print("GitHub Test2")
+while GAME_OVER != True:
+    nihgt_counter += 1
+    print(nihgt_counter)
+    if nihgt_counter == 10:
+        GAME_OVER = True
